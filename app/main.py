7a6a60a22867180ee0e2c3fd8a3a9e313f7cb427
Kami <josephkiarie561@gmail.com>
2025-08-05 12:11:52 +0300

    loading the model

--- app/main.py
+++ app/main.py
@@ -1,3 +1,9 @@
 
 # streamlit application
 import streamlit as st
+import pandas as pd
+import pickle
+import numpy as np
+
+model = pickle.load(open("notebook/model.sav", "rb"))
+scaler = pickle.load(open("notebook/scaler.sav", "rb"))
